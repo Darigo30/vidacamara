36918434717c8b5b516223ce18831c1bc33a2307
darigo30 <darigomez30@gmail.com>
2021-07-29 23:48:45 -0400

    finalizacion de index, inicio de modal y responsive design

---
+++ .history/js/custom_20210729183006.js
@@ -0,0 +1,14 @@
+var accordion = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < accordion.length; i++) {
+  accordion[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    } 
+  });
+}
